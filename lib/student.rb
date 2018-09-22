4d9ad0e99c3d18225da5896d50af101e0b78d836
Tammy Metz <tammy@tammymetz.com>
2018-09-22 16:10:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,6 @@
 class Student
+
+  @@all = []
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
@@ -13,12 +15,14 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+
     sql = <<-SQL
       SELECT * FROM students
     SQL
     DB[:conn].execute(sql) do |row|
-      self.new_from_db(row)
+      @@all << self.new_from_db(row)
     end
+    @@all
   end
 
   def self.find_by_name(name)
